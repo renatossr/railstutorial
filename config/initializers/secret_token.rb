bd905407d606481264d9282f47819aa7faf84815
Renato Ribeiro <renatoribeiro@Renatos-MacBook-Air.local>
2013-07-29 15:29:50 +0800

    Changed Gitignore

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,21 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '4918fabffee77df2a793310a1b9dd325496cdc83a076bd62c50037c80d01f10a78c16822dfaa35aa21edd8f9eccda686a1083e70509fb553ab363864dbd4ac2b'
+require 'securerandom'
+
+def secure_token
+  
+  token_file = Rails.root.join('.secret')
+  
+  if File.exist?(token_file)
+  	# Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+	end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
